9f66180f931e3a931bb3fd208c8ab41e9eb3ea33
Danil Grigorev <danil.grigorev@suse.com>
2023-09-26 14:03:10 +0200

    Prepare a preview for pages deployment on PR
    Signed-off-by: Danil Grigorev <danil.grigorev@suse.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.rancher-turtles.com',
+  url: process.env.URL ? `${process.env.URL.toLowerCase()}` : "https://docs.rancher-turtles.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL ? `${process.env.BASE_URL}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
